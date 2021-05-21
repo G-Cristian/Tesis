268ca8ac8f1b3d0cc1038034d2bd983fc21338d0
Cris <registro.paginas2@gmail.com>
2021-05-21 17:55:07 -0300

    Agregado código para que cree otro archivo de log más completo que incluye los prints del programa.

--- ICML2021-main/neuralImplicitTools/src/Excecute_Importance.sh
+++ ICML2021-main/neuralImplicitTools/src/Excecute_Importance.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-prefix_name="Surface"
+prefix_name="Surface-FPS"
 date_time=$(date +"%Y_%m_%d_%H_%M_%S")
 
 name=$prefix_name'_'$date_time
 outputDir="../results/$name"
 
 logfile="../results/$name/log.txt"
-train_args="--outputDir $outputDir --samplingMethod $prefix_name --epochLengthPow 3"
+completeLogfile="../results/$name/complete_log.txt"
+train_args="--outputDir $outputDir --samplingMethod $prefix_name --epochLengthPow 5"
 #train_args="--outputDir $outputDir --epochLengthPow 6 --epochs 1000 --writeOutEpochs 1"
 #train_args="--outputDir $outputDir --showVis 1 --reconstructionRes 128"
 
@@ -22,37 +23,58 @@ mkdir "$outputDir"
 echo "Creando archivo de log $logfile"
 touch "$logfile"
 
+echo "Creando archivo de log completo $completeLogfile"
+touch "$completeLogfile"
+
+echo "Inicio -- $(date +"%Y_%m_%d_%H_%M_%S") --"
 echo "Inicio -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$logfile"
+echo "Inicio -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$completeLogfile"
 
 for obj_file in ../data/*.obj; do
     [ -f "$obj_file" ] || break
 
     echo "Ejecutando trainer.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..."
     echo "Ejecutando trainer.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..." >> "$logfile"
+    echo "Ejecutando trainer.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..." >> "$completeLogfile"
+    
     echo "python3 trainer.py ../data/$obj_file $train_args"
-    python3 trainer.py ../data/$obj_file $train_args
+    echo "python3 trainer.py ../data/$obj_file $train_args" >> "$logfile"
+    echo "python3 trainer.py ../data/$obj_file $train_args" >> "$completeLogfile"
+
+    python3 trainer.py ../data/$obj_file $train_args >> "$completeLogfile"
 
     echo "... Fin ejecutando trainer.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --"
     echo "... Fin ejecutando trainer.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$logfile"
+    echo "... Fin ejecutando trainer.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$completeLogfile"
     
     echo "Ejecutando modelmesher.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..."
     echo "Ejecutando modelmesher.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..." >> "$logfile"
+    echo "Ejecutando modelmesher.py para $obj_file ($(date +"%Y_%m_%d_%H_%M_%S")) ..." >> "$completeLogfile"
     
-    python3 modelmesher.py $outputDir
+    echo "python3 modelmesher.py $outputDir"
+    echo "python3 modelmesher.py $outputDir" >> "$logfile"
+    echo "python3 modelmesher.py $outputDir" >> "$completeLogfile"
+    
+    python3 modelmesher.py $outputDir >> "$completeLogfile"
     
     echo "... Fin ejecutando modelmesher.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --"
     echo "... Fin ejecutando modelmesher.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$logfile"
+    echo "... Fin ejecutando modelmesher.py para $obj_file -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$completeLogfile"
 done
 
 #echo "Ejecutando metrics.py ($(date +"%Y_%m_%d_%H_%M_%S")) ..."
 #echo "Ejecutando metrics.py ($(date +"%Y_%m_%d_%H_%M_%S")) ..."  >> "$logfile"
-#python3 metrics.py $outputDir ../data/
+#echo "Ejecutando metrics.py ($(date +"%Y_%m_%d_%H_%M_%S")) ..."  >> "$completeLogfile"
+
+#python3 metrics.py $outputDir ../data/ >> "$completeLogfile"
 
 #echo "... Fin ejecutando metrics.py -- $(date +"%Y_%m_%d_%H_%M_%S") --"
 #echo "... Fin ejecutando metrics.py -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$logfile"
+#echo "... Fin ejecutando metrics.py -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$completeLogfile"
 
 echo "Fin -- $(date +"%Y_%m_%d_%H_%M_%S") --"
 echo "Fin -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$logfile"
+echo "Fin -- $(date +"%Y_%m_%d_%H_%M_%S") --" >> "$completeLogfile"
 
 exit
 
